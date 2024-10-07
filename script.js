b8ede5cbad08059ac3b250f92ee6cfe971765222
Durva1206 <pithwadurva4@gmail.com>
2024-10-07 14:43:16 +0530

    javascrip

---
+++ script.js
@@ -0,0 +1,34 @@
+var name = "Durva";
+var num1 = 10;
+var num2;
+var value = true;
+var array = [1, 2, 3, 4, 5, "Durva", [10, 20, 30]];
+var num3 = null;
+
+var person = {name: "Durva", 
+                age: 19,
+                city: "Vadodara",
+                alive:true };
+
+let vehicals = {car:  "BMW", 
+                bike: "Honda", 
+                truck: "Tata"};
+
+
+console.log(name);
+console.log(num1);
+console.log(num2);
+console.log(value);
+console.log(array);
+console.log(num3);
+console.log(person);
+console.log(vehicals);
+console.log(person.age);
+console.log(person.name);
+console.log(person.city);
+console.log(typeof name);
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof value);
+console.log(typeof array);
+console.log(typeof num3);
